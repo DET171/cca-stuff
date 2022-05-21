ac27b71975eb48d12431e73f7c5df53797e6fd67
DET171 <mrgeek484@gmail.com>
2022-05-21 10:07:12 +0800

    gg

--- todos-app/src/App.vue
+++ todos-app/src/App.vue
@@ -1,21 +1,34 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+	<ul>
+		<Todo v-for="todo in todos" :key="todo.id" :todo="todo" />
+	</ul>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+import axios from 'axios';
+
+export default {
+	data: () => ({
+		todos: [],
+	}),
+	created() {
+		this.fetchTodos();
+	},
+	methods: {
+		async fetchTodos() {
+			const todos = (await axios.get('https://jsonplaceholder.typicode.com/todos')).data.slice(0, 15);
+			this.todos = todos;
+		},
+	},
+};
+</script>
+
+<style lang="less" scoped>
+* {
+	font-family: sans-serif;
 }
 </style>
---
+++ todos-app/src/components/Todo.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps({
+	todo: Object,
+});
+</script>
+
+<template>
+	<h1 :class="{ done: todo.completed, todo }">{{ todo.id }}: {{ todo.title }}</h1>
+</template>
+
+<script>
+export default {
+	name: 'Todo',
+};
+</script>
+
+<style lang="less" scoped>
+.undone {
+	background-color: crimson;
+	color: white;
+	border: 5px solid rgb(153, 3, 33);
+}
+.todo {
+	padding: 30px;
+	margin: 20px auto;
+	width: 50%;
+	.undone();
+}
+.done {
+	background-color: chartreuse;
+	border: 5px solid rgb(87, 173, 0);
+}
+</style>
